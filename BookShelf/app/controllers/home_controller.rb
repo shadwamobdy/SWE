5a3f7f6f8c6a1af45d573616dfee24bba60dff3d
Unknown <shadwa.mobdy@gmail.com>
2018-04-26 13:49:07 +0200

    Milestone 3
    Updates on book part

--- BookShelf/app/controllers/home_controller.rb
+++ BookShelf/app/controllers/home_controller.rb
@@ -11,6 +11,7 @@ class HomeController < ApplicationController
   			@books[c] = Book.where(category: c)
   			if !@books[c].any?
   				@books[c] = GoogleBooks.search("subject: #{c}")
+  				puts "----------------------> size #{@books[c].any?} category #{c}"
   				@books[c].each do |b|
   					book = Book.new(:title => b.title, :description => b.description, :image_link => b.image_link, :category => c)
   					if !book.save!
@@ -22,6 +23,13 @@ class HomeController < ApplicationController
   	end
 
   	def search
+  		filter = params[:filter]
+  		if !(filter)
+  			
+  		else
+  			 
+  		end
+  			
   	end
 
   	def admin
