a1dca10a493afc74d10944b174f564ebbe8ff192
Salma Attia <salmamohamedattia@gmail.com>
2018-04-28 03:36:19 +0200

    “Add”

--- BookShelf/app/models/book.rb
+++ BookShelf/app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ActiveRecord::Base
+	validates :isbn, presence: true
+	validates :created_at, presence: true 
+	validates :updated_at, presence: true 
+	validates_uniqueness_of :updated_at
+	def index 
+		
+	end
 end
